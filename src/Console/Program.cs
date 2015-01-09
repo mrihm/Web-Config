a311297a535d2b22c516e307aff651516cff8e1d
mrihm <mischa.rihm@gmail.com>
2015-01-08 23:46:03 -0600

    Configuration implementation

---
+++ src/Console/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Configuration;
+
+using Config.ConsoleApp.Configuration;
+
+namespace Config.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var settings = ConfigurationManager.GetSection("smtp") as SmtpConfiguration;
+
+            Console.WriteLine(settings.Client.Host);
+            Console.WriteLine((settings.Client.Credentials as System.Net.NetworkCredential).UserName);
+            Console.WriteLine("{0}\n", (settings.Client.Credentials as System.Net.NetworkCredential).Password);
+
+            Console.WriteLine(settings.Sender.Address);
+            Console.WriteLine("{0}\n", settings.Sender.DisplayName);
+
+        }
+    }
+}
---
+++ src/Library/Configuration/ISmtpSettings.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Net.Mail;
+
+namespace Config.Library.Settings
+{
+    public interface ISmtpSettings
+    {
+        SmtpClient Client { get; }
+        MailAddress Sender { get; }
+        MailAddress ReplyTo { get; }
+    }
+}
